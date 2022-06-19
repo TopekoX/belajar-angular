cb32137747a13943ddde86f11234f4573eb2d5c3
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-19 13:15:47 +0800

    directive: ngIf

--- basic/src/app/servers/servers.component.ts
+++ basic/src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false
   serverCreationStatus = 'No Server was Created'
   serverName = 'Test Server'
+  serverCreated = false
 
   constructor() {
     setTimeout(() => {
@@ -26,6 +27,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true
     this.serverCreationStatus = 'Server was Created! name is ' + this.serverName
   }
 
