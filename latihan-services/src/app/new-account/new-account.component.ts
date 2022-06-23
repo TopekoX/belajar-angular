74acfd44e296aad4efdf66490bef6a9e30d10092
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-23 22:56:33 +0800

    Injection Services (Logging Services)

--- latihan-services/src/app/new-account/new-account.component.ts
+++ latihan-services/src/app/new-account/new-account.component.ts
@@ -4,12 +4,13 @@ import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent implements OnInit {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>()
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }
 
   ngOnInit(): void {
   }
@@ -19,9 +20,7 @@ export class NewAccountComponent implements OnInit {
       name: accountName,
       status: accountStatus
     })
-    const service = new LoggingService()
-    service.logStatusChangger(accountStatus)
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChangger(accountStatus)
   }
 
 }
