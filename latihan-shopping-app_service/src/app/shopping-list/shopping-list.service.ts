a71d1c62a31ba9b6e1c1fc3f11d437163d14b9a2
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-24 19:25:20 +0800

    add shoppinglist service

--- latihan-shopping-app_service/src/app/shopping-list/shopping-list.service.ts
+++ latihan-shopping-app_service/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,22 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
+    ingredientChanged = new EventEmitter<Ingredient[]>()
+    private ingredients: Ingredient[] = [
+        new Ingredient('Daging Sapi', 2),
+        new Ingredient('Daging Lembu', 5)
+    ]
+
+    getIngredients() {
+        // return the ingredients array
+        return this.ingredients.slice()
+    }
+
+     addIngredients(ingredient: Ingredient) {
+        this.ingredients.push(ingredient)
+        this.ingredientChanged.emit(this.ingredients.slice())
+    }
     
 }
+ 
