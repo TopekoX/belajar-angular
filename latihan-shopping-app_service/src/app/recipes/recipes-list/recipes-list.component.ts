35853bde70d25de8b867ce65bb7e477d3442f4fd
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-24 19:02:49 +0800

    add RecipeService

--- latihan-shopping-app_service/src/app/recipes/recipes-list/recipes-list.component.ts
+++ latihan-shopping-app_service/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,21 +8,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>()
-  recipes: Recipe[] = [
-    new Recipe('Coto Makassar', 'Makanan Khas Daerah Makassar, Sulawesi Selatan', 
-              'https://asset.kompas.com/crops/ljY1r3f3HJaNEuLip3iARQOaK-E=/0x1226:3000x3226/750x500/data/photo/2021/06/11/60c2acf88c6f0.jpg'),
-              new Recipe('Kaledo', 'Makanan Khas Kota Palu, Sulawesi Tengah', 
-              'https://upload.wikimedia.org/wikipedia/commons/e/eb/Kaledo_soup%2C_Central_Sulawesi%2C_2018-09-16.jpg')
-  ]
-
-  constructor() { }
+  recipes: Recipe[] | undefined
+  
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-     this.recipeWasSelected.emit(recipe)
+    this.recipes = this.recipeService.getRecipe()
   }
 
 }
