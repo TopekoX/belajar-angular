815b99b1a8a8229a50f7b8aa994412001fc92e32
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-26 16:29:46 +0800

    first init

---
+++ latihan-routes/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UserComponent } from './users/user/user.component';
+import { FormsModule } from '@angular/forms';
+import { ServersService } from './servers/servers.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    HomeComponent,
+    ServersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [ServersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
