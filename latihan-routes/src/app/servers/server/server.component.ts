e153bd588f0a134a71d49320e66f70ee596afc18
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-27 17:34:04 +0800

    child nested route

--- latihan-routes/src/app/servers/server/server.component.ts
+++ latihan-routes/src/app/servers/server/server.component.ts
@@ -15,9 +15,7 @@ export class ServerComponent implements OnInit {
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id']
-    this.server = this.serversService.getServer(id);
-    console.log(this.server?.status);
-    
+    this.server = this.serversService.getServer(id); 
     this.route.params.subscribe(
       (param: Params) => {
         this.server = this.serversService.getServer(+param['id']);    
