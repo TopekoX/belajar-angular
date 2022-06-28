d25e866d8c4eb59a3e4a4c26d7b874aba0c12c69
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-28 18:28:50 +0800

    control navigation with canDeactive

--- latihan-routes/src/app/home/home.component.ts
+++ latihan-routes/src/app/home/home.component.ts
@@ -19,7 +19,7 @@ export class HomeComponent implements OnInit {
     this.router.navigate(
       ['/servers', id, 'edit'],
       {
-        queryParams: {allowEdit: 1},
+        queryParams: {allowEdit: '1'},
         fragment: 'loading'
       })
   }
--- latihan-routes/src/app/servers/edit-server/edit-server.component.ts
+++ latihan-routes/src/app/servers/edit-server/edit-server.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ServersService } from '../servers.service';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate  {
   server: { id: number; name: string; status: string; } | undefined;
   serverName = '';
   serverStatus = '';
   allowEdit = false
+  changeSave = false
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
@@ -34,6 +38,20 @@ export class EditServerComponent implements OnInit {
   onUpdateServer() {
     this.serversService.updateServer(this.server!.id, 
       {name: this.serverName, status: this.serverStatus});
+    this.changeSave = true
+    this.router.navigate(['../'], {relativeTo: this.route})
+  }
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true
+    } 
+    if ((this.serverName !== this.server?.name || this.server.status !== this.serverStatus) &&
+         !this.changeSave) {
+          return confirm('Do you want to discard changes?')
+    } else {
+      return true
+    }
   }
   
 }
