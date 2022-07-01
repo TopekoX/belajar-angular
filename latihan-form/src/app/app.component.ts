a291f0aef6e61d1ec63bd36a1ed407d327cf1e96
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-01 08:37:04 +0800

    Template Driven: Access element form using NgForm

--- latihan-form/src/app/app.component.ts
+++ latihan-form/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  onSubmit(form: HTMLFormElement) {
+  onSubmit(form: NgForm) {
     // console.log('Submitted!!!');
     console.log(form);   
   }
