b149aa7b9fabbd6a51b0fb88811b6fdfa1b458c6
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-01 10:59:53 +0800

    Template Driven: Patching form value

--- latihan-form/src/app/app.component.ts
+++ latihan-form/src/app/app.component.ts
@@ -21,4 +21,25 @@ export class AppComponent {
     console.log(this.signupForm);
   }
 
+  suggestUserName() {
+    const suggestName = 'Superuser'
+    /* pakai setValue kalau data yg ingin diganti berupa group */
+    // this.signupForm?.setValue({
+    //   userData: {
+    //     username: suggestName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+
+    /* pakai patchValue kalau data yang ingin diganti cuma bagian field tertentu sj */
+    this.signupForm?.form.patchValue({
+      userData: {
+        username: suggestName
+      }
+    })
+  }
+
 }
