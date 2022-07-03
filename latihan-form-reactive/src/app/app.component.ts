72be45d8af462ae383b0b655c0c7f512e81e4cfa
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-03 22:44:16 +0800

    using Form Array

--- latihan-form-reactive/src/app/app.component.ts
+++ latihan-form-reactive/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +7,34 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female']
-  signupForm!: FormGroup
- 
+  genders = ['male', 'female'];
+  signupForm!: FormGroup;
+
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     })
   }
 
   onSubmit() {
-    console.log(this.signupForm)
+    console.log(this.signupForm);
+  }
+
+  onAddHobbies() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // method tambahan untuk men-convert ke nilai array agar bisa di proses nilai controlsnya
+  // ketika dipanggil pada saat iterasi di page html    
+  // read more: https://www.angularfix.com/2022/02/property-does-not-exist-on-type_18.html
+  get hobbies(): FormArray {
+    return this.signupForm.get('hobbies') as FormArray;
   }
 
 }
