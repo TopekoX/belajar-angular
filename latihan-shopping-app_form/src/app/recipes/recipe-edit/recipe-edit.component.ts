adda5b85b4d83e4978b9e8ec51f8bf028ecbe9e8
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-05 22:50:17 +0800

    sync html with form recipes edit

--- latihan-shopping-app_form/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ latihan-shopping-app_form/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,10 +9,12 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-  id: number | undefined
+  id!  : number
   editMode: boolean = false
+  recipeForm!: FormGroup
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params
@@ -19,9 +23,32 @@ export class RecipeEditComponent implements OnInit {
             this.id = +params['id']
             this.editMode = params['id'] != null
             console.log(this.editMode);
-            
+            this.initForm()
           }
         )
+  }
+
+  private initForm() {
+    let recipeName = ''
+    let recipeImagePath = ''
+    let recipeDescription = ''
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id) 
+      recipeName = recipe.name
+      recipeImagePath = recipe.imagePath
+      recipeDescription = recipe.description
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription)
+    })
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm)
   }
 
 }
