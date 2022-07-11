49c2b092b7ca86146e324724b411645d38820122
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-11 22:05:22 +0800

    using subject for error handling

--- latihan-http/src/app/posts.service.ts
+++ latihan-http/src/app/posts.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { map } from "rxjs/operators";
 import { Post } from "./post.model"
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
+    error = new Subject<string>()
 
     constructor(private http: HttpClient) { }
 
@@ -16,6 +18,8 @@ export class PostsService {
             postData
         ).subscribe(responseData => {
             console.log(responseData)
+        }, error => {
+            this.error.next(error.message)
         })
     }
 
