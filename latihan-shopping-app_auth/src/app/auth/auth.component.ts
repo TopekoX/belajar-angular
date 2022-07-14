fef477596e25260ddd0d50b1b613974292183d4b
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-14 18:13:33 +0800

    handling form input

--- latihan-shopping-app_auth/src/app/auth/auth.component.ts
+++ latihan-shopping-app_auth/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { style } from "@angular/animations";
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
     selector: 'app-auth',
@@ -11,5 +12,10 @@ export class AuthComponent {
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode
+    }
+
+    onSubmit(form: NgForm) {
+        console.log(form.value)
+        form.reset()
     }
 }
