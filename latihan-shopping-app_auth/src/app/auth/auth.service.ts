f36e5c1dd4fc6c98c3dce17fd8ff65460f60c0da
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-15 10:43:48 +0800

    creating and storing user data

--- latihan-shopping-app_auth/src/app/auth/auth.service.ts
+++ latihan-shopping-app_auth/src/app/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, throwError } from "rxjs";
+import { catchError, Subject, tap, throwError } from "rxjs";
+import { User } from "./user.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
+    user = new Subject<User>()
     
     constructor(private http: HttpClient) { }
 
@@ -17,7 +19,13 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError))
+        ).pipe(catchError(this.handleError), tap(responseData => {
+            this.handleAuthentication(
+                responseData.email, 
+                responseData.localId, 
+                responseData.idToken, 
+                +responseData.expiresIn)
+        }))
     }
 
     login(email: string, password: string) {
@@ -28,7 +36,23 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true    
             }
-        ).pipe(catchError(this.handleError))
+        ).pipe(catchError(this.handleError), tap(responseData => {
+            this.handleAuthentication(
+                responseData.email, 
+                responseData.localId, 
+                responseData.idToken, 
+                +responseData.expiresIn)
+            }))
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
+            const user = new User(
+                email, 
+                userId, 
+                token, 
+                 expirationDate)
+            this.user.next(user)
     }
 
     private handleError(errorResponse: HttpErrorResponse) {
