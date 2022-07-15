6479d31bbf385daf14e239e1bcc8381ab93405f6
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-15 18:40:29 +0800

    auto login - save userdata to localstorage

--- latihan-shopping-app_auth/src/app/auth/auth.service.ts
+++ latihan-shopping-app_auth/src/app/auth/auth.service.ts
@@ -46,6 +46,29 @@ export class AuthService {
             }))
     }
 
+    autoLogin() {
+         const userData: {
+            email: string
+            id: string
+            _token: string
+            _tokenExpirationDate: string
+         } = JSON.parse(localStorage.getItem('userData')!)
+
+         if (!userData) {
+            return
+         }
+         const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+        )
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser )
+        }
+    }
+
     logout() {
         this.user.next(null as any)
         this.router.navigate(['/auth'])
@@ -59,6 +82,7 @@ export class AuthService {
                 token, 
                  expirationDate)
             this.user.next(user)
+            localStorage.setItem('userData', JSON.stringify(user))
     }
 
     private handleError(errorResponse: HttpErrorResponse) {
