b540d4181e18ff1837e1b5a06b409d11cb60b543
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-15 09:47:56 +0800

    sending login request

--- latihan-shopping-app_auth/src/app/auth/auth.service.ts
+++ latihan-shopping-app_auth/src/app/auth/auth.service.ts
@@ -29,12 +29,24 @@ export class AuthService {
             return throwError(() => errorMessage)
         }))
     }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyACCrF-qa7kAPQy6-weVzG1Y0sXtQ4A1UI',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true    
+            }
+        )
+    }
 }
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string
     email: string
     refreshToken: string
     expiresIn: string
     localId: string
+    registered: boolean
 }
