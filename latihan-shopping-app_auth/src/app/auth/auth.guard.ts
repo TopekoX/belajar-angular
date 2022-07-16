2131c06f9671e709c5a0fbac335584cef9f52b1c
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-16 12:32:47 +0800

    addind auth guard 2

--- latihan-shopping-app_auth/src/app/auth/auth.guard.ts
+++ latihan-shopping-app_auth/src/app/auth/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { map, Observable, tap } from "rxjs";
+import { map, Observable, take, tap } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable({
@@ -9,15 +9,25 @@ import { AuthService } from "./auth.service";
 export class AuthGuard implements CanActivate {
 
     constructor(private autService: AuthService, private router: Router) { }
-     
+
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.autService.user.pipe(map(user => {
-            return !!user
-        }), tap(isAuth => {
-            if (!isAuth) {
-                this.router.navigate(['/auth'])
+        return this.autService.user.pipe(
+            take(1),
+            map(user => {
+            const isAuth = !!user
+            if (isAuth) {
+                return true
             }
-        }))
+            // cara:  1
+            return this.router.createUrlTree(['/auth'])
+        }),
+            // cara: 2
+            // tap(isAuth => {
+            //     if (!isAuth) {
+            //         this.router.navigate(['/auth'])
+            //     }
+            // })
+        )
     }
-    
+
 }
