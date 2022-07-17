0bc35fd88b426d8a5f113b1703a06fd8aa96aab9
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-17 21:34:07 +0800

    more lazy loading (shopping-list & auth)

--- latihan-shopping-app_modules/src/app/app-routing.module.ts
+++ latihan-shopping-app_modules/src/app/app-routing.module.ts
@@ -7,6 +7,16 @@ const appRoutes: Routes = [
         path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(module => 
             module.RecipesModule
         )
+    },
+    {
+        path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(module => 
+            module.ShoppingListModule
+        )
+    },
+    {
+        path: 'auth', loadChildren: () => import('./auth/auth.module').then(module => 
+            module.AuthModule
+        )
     }
 ]
 
