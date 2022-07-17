2b47139b2e167f43edd84788d37fc30afee3348f
Ucup TopekoX <ucup.topekox@gmail.com>
2022-07-17 21:22:31 +0800

    add lazy loading

--- latihan-shopping-app_modules/src/app/app-routing.module.ts
+++ latihan-shopping-app_modules/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
-     { path: '', redirectTo: '/recipes', pathMatch: 'full' }
-] 
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    {
+        path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(module => 
+            module.RecipesModule
+        )
+    }
+]
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
